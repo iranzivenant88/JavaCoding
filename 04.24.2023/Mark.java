9408e31b5014353df5d1d2730c9da69b7f1635bd
iranzivenant88 <88venant@gmail.com>
2023-05-06 22:30:13 -0600

    my changes

---
+++ 04.24.2023/Mark.java
@@ -0,0 +1,18 @@
+class AA{
+	public static void main(String []args){
+	System.out.println("Class AA");
+	}
+}
+class BB{
+	public static void main(String [] args){
+	System.out.println("Class BB");
+
+	}
+}
+
+class CC{
+	public static void main(String[]args){
+	System.out.println("Class CC");
+	}
+}
+class DD{}
---
+++ 04.24.2023/Steve.java
@@ -0,0 +1,22 @@
+class AAA{
+	public static void main(String[]args){
+	System.out.println("AAA class");
+	}
+}
+
+class BBB{
+	public static void main(String[]args){
+	System.out.println("BBB class");
+	}
+}
+
+class CCC{
+	public static void main(String[]args){
+	System.out.println("CCC class");
+	}
+}
+class DDD{
+	public static void main(String[]args){
+	System.out.println("DDD class");
+	}
+}
