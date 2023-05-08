763dca5bc075a8463cc7513b570559c5c78144a1
iranzivenant88 <88venant@gmail.com>
2023-05-08 11:47:24 -0600

    all changes

---
+++ 05.07.2023/Expression.java
@@ -0,0 +1,15 @@
+public class Expression{
+	public static void main(String[]args){
+	String a ="James";
+	int b =10, c =20,d =30;
+	a =b+c+d;
+	System.out.println(a);
+	a =a+b+c;
+	System.out.println(a);
+	b =a+c+d;
+	System.out.println(b);
+
+	b =b+c+d;
+	System.out.println(b);
+	}
+}
---
+++ 05.07.2023/PlusOperatorExTwo.java
@@ -0,0 +1,10 @@
+public class PlusOperatorExTwo{
+	public static void main(String[]args){
+	String a ="James";
+	int b = 20, c = 30,d = 40;
+	System.out.println(a+b+c+d);
+	System.out.println(b+c+d+a);
+	System.out.println(b+c+a+d);
+	System.out.println(b+a+c+d);
+	}
+}
