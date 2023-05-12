097d9f084bb37ac2e3beeb7a03f9000b2e9f95b3
iranzivenant88 <88venant@gmail.com>
2023-05-11 23:21:19 -0600

    changes

---
+++ 05.09.2023/public class OperatorTest{.java
@@ -0,0 +1,18 @@
+public class OperatorTest{
+	public static void main(String[]harris){
+	 System.out.println("Start of the main method");
+	 System.out.println(56^78);
+	 int i = 56;
+	 int j = 45;
+	 if(++i> 89 && --j<90){
+	 	System.out.println("i");
+
+	 }else{
+	 	System.out.println(89|88);
+	 	System.out.println(45^49);
+	 }
+	 System.out.println(66^6);
+	 System.out.println("End of main method");
+
+	}
+}
---
+++ 05.09.2023/public class PlusOperatorExOne{.java
@@ -0,0 +1,11 @@
+public class PlusOperatorExOne{
+	public static void main(String[]args){
+	int a = 10;
+	int b = 20;
+	String firstName = "James";
+	String lastName  = "Gosling";
+	System.out.println(a+b);
+	System.out.println(firstName + lastName);
+	System.out.println('a'+0.89);
+	}
+}
