c048ceb74fd7529bd5466aa9dec7662af739d452
iranzivenant88 <88venant@gmail.com>
2023-05-29 23:39:47 -0600

    changes

---
+++ Test/AccountTesting.java
@@ -0,0 +1,15 @@
+package com.account.testing;
+import org.account.Account;
+
+public class AccountTesting{
+public static void main(String[]args){
+	System.out.println("Start of main method");
+	Account accountOne = new Account(111,"Venant",9000.00);
+	Account accountTwo = new Account(222,"Abhajah",9000.00);
+	accountOne.n2();
+	accountTwo.n1();
+	System.out.println("End of main method");
+}
+
+}
+///Users/venantnsabimana/Downloads/AccountOne
