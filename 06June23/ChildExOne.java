18d4542dc61af499d98ccba1e355885ad4e7a02c
iranzivenant88 <88venant@gmail.com>
2023-06-12 22:09:51 -0600

    changes

---
+++ 06June23/ChildExOne.java
@@ -0,0 +1,10 @@
+package com.us.venant;
+import org.venu.packagetest.ParentExOne;
+public class ChildExOne {
+	public static void main(String... args){
+		ChildExOne ceo = new ChildExOne();
+		System.out.println(ceo.age);
+		System.out.println(ceo.name);
+		ceo.r1();
+	}
+}
---
+++ 06June23/ParentExOne.java
@@ -0,0 +1,8 @@
+package org.venu.packagetest;
+public class ParentExOne{
+	public int age = 34;
+	public String name = "venant";
+	public void r1(){
+	System.out.println("r1- non static method");
+	}
+}
