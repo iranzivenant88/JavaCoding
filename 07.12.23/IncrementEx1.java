e6ef3fa0a8078cd94017b8d67a2309e76deda0a7
iranzivenant88 <88venant@gmail.com>
2023-07-12 23:49:14 -0600

    changes

--- 07.12.23/IncrementEx1.java
+++ 07.12.23/IncrementEx1.java
@@ -1,9 +1,9 @@
 public class IncrementEx1{
 	public static void main(String...args){
-		int a = 23;
-		int b =(++a)a;
+		int i = 23;
+		int j =(++j)j;
 
-		System.out.println("The value of a is :"+a);
-		System.out.println("The value of b is :"+b);
+		System.out.println("The value of a is :"+j);
+		System.out.println("The value of b is :"+j);
 	}
 }
