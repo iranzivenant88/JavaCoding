33b0e33470d3f780ddbbb25d3228ef6b87504034
iranzivenant88 <88venant@gmail.com>
2023-07-15 11:51:20 -0600

    Changes

---
+++ 07.12.23/PrimitiveVar.java
@@ -0,0 +1,8 @@
+public class PrimitiveVar{
+	public static void main(String...args){
+	int a = 13;
+	int b = a;
+	a = 40;
+	System.out.println("The value of a is : "+a +" and the value of b is : "+b);
+}
+}
