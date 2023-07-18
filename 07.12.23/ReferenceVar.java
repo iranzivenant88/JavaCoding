29994ddbf6b953e11196efb917d7200549359524
iranzivenant88 <88venant@gmail.com>
2023-07-17 22:18:27 -0600

    changes

--- 07.12.23/ReferenceVar.java
+++ 07.12.23/ReferenceVar.java
@@ -1,15 +1,29 @@
 public class ReferenceVar{
 	int age = 12;
 	
-	public Reference(int age){
+	public ReferenceVar(int age){
 		this.age = age;
-
 		System.out.println("The age is :"+age);
 	}
+	public static void main(String...args){
+		Person alex = new Person("Alex");
+		Person Clement = alex;
 
-	public void make(){
-		System.out.println("Make method");
+		System.out.println("Before changing alex");
+		System.out.println(alex.name +" | "+Clement.name);
+
+		alex.name = "venant";
+		System.out.println("after changing alex");
+		System.out.println(alex.name +" | "+Clement.name);
+
+
+	}
+	
+}
+ class Person{
+	String name;
+	Person(String name){
+		this.name = name;
 	}
 
-	
 }
