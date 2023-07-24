c66101567297461ad2fbc8c1adb59ca3c43b4c68
iranzivenant88 <88venant@gmail.com>
2023-07-23 21:11:12 -0600

    changes

--- 07.12.23/MaxClass.java
+++ 07.12.23/MaxClass.java
@@ -3,18 +3,17 @@ public class MaxClass {
 	public static void main(String[]args){
 		int [] numbers = {2,4,7,2,4};	
 		int max = getMaxNumber(numbers);
-		System.out.println("The max number is :"+max);
-	
-}
+		System.out.println("The max number is : "+max);
+	}
 	public static int getMaxNumber(int[]numbers){
-		int maxNumber = numbers[0];
+		int maxNum = numbers[0];
 		for(int i = 0;i<numbers.length;i++){
-			if(numbers[i]>maxNumber){
-				maxNumber = numbers[i];
+			if(numbers[i]>maxNum){
+				maxNum = numbers[i];
 			}
 		}
 
-		return maxNumber;
+		return maxNum;
 	}
 		
 }
