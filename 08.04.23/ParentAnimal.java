98e3e131b7acd7cf0c11e03aba9d97692bcacf9c
iranzivenant88 <88venant@gmail.com>
2023-08-08 23:33:20 -0600

    Changes

---
+++ 08.04.23/ParentAnimal.java
@@ -0,0 +1,12 @@
+public class ParentAnimal{
+	int age = 12;
+
+	public void getAge(int age){
+	this.age = age;
+		System.out.println("The age is :"+age);
+	}
+	public int setAge(){
+		return age;
+	}
+
+}
