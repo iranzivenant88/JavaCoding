a172ebb71dbf5efff43acb96f73c71f1c7fba83d
iranzivenant88 <88venant@gmail.com>
2023-08-12 23:14:38 -0600

    changes

---
+++ August-2023/08.11.23/ArrayExFive.java
@@ -0,0 +1,9 @@
+package com.org.arrayexample.five;
+public class ArrayExFive{
+	public static void main(String...args){
+		System.out.println("Start of main method");
+		int[]t = new int[21];
+		System.out.println(t);
+		System.out.println("End of main method");
+	}
+}
