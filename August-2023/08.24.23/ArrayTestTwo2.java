f8bf1ebc5c628a3231b556a09d501e2263723c1c
iranzivenant88 <88venant@gmail.com>
2023-08-26 23:45:29 -0600

    Changes

---
+++ August-2023/08.24.23/ArrayTestTwo2.java
@@ -0,0 +1,14 @@
+package org.test.arrayexampleone;
+public class ArrayTestTwo2{
+	public static void main(String...args){
+	System.out.println("Start of main method");
+		int[]arrayOne = {40,50,20,10};
+		System.out.println(arrayOne);
+		System.out.println(arrayOne.length);
+		for(int index = 0;index<arrayOne.length;index++){
+			System.out.println("The index is  :"+index);
+			System.out.println("The element is :"+arrayOne[index]);
+		}
+		System.out.println("End of main method");
+	}
+}
