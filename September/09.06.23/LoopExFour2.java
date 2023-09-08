ec51c395b3807198dbe8960aed9c7afc33720545
iranzivenant88 <88venant@gmail.com>
2023-09-07 21:12:05 -0600

    cganes

--- September/09.06.23/LoopExFour2.java
+++ September/09.06.23/LoopExFour2.java
@@ -1,5 +1,5 @@
 package org.test.loopexamplefour;
-	public class LoopExFour{
+	public class LoopExFour2{
 	public static void main(String...args){
 		System.out.println("Start of main method");
 			while(1)
