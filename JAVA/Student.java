2e1b20c3c650b92192cdc03b24f0dbbd112fb5e1
iranzivenant88 <88venant@gmail.com>
2023-09-22 23:29:06 -0600

    changes

---
+++ JAVA/Student.java
@@ -0,0 +1,35 @@
+public class Student{
+	private int studentId;
+	private String name;
+	private float qualifyingExamMarks;
+	private int yearOfEngg;
+
+	public int getStudentId(){
+		return studentId;
+		
+	}
+	public void setStudentId(int studentId){
+		this.studentId = studentId;
+	}
+	public String getName(){
+		return name;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public float getQualifyingExamMarks(){
+		return qualifyingExamMarks;
+		
+	}
+	public void setQualifyingExamMarks(float qualifyingExamMarks){
+		this.qualifyingExamMarks = qualifyingExamMarks;
+	}
+	public int getYearOfEngg(){
+		return yearOfEngg;
+		
+	}
+	public void setyearOfEngg(int yearOfEngg){
+		this.yearOfEngg = yearOfEngg;
+	}
+}
