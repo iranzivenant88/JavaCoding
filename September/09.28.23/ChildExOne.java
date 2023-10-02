3d8a91ded5297085379c57811a72099bccb3d647
iranzivenant88 <88venant@gmail.com>
2023-10-01 23:08:04 -0600

    changes

---
+++ September/09.28.23/ChildExOne.java
@@ -0,0 +1,10 @@
+package com.venant.example;
+public abstract class   ChildExOne{
+	public static void main(String...args){
+	System.out.println("Start of main method");
+	System.out.println("This concept was introduced in jdk 8");
+	System .out.println("End of main method");
+
+	}
+	 
+}
